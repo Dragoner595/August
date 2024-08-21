e688f541ca8f339024e9043b65c58c73e56a5606
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-08-21 14:26:10 +0000

    modified:   amazon_analyzis.py commit

--- amazon_analyzis.py
+++ amazon_analyzis.py
@@ -27,3 +27,31 @@ average = data['length'].mean()
 closest_index = (data['length'] - average).abs().idxmin()
 
 print(data.loc[closest_index]['verified_reviews'])
+
+positive = data[data['feedback'] == 1 ]
+
+negative =data[data['feedbacl'] == 0]
+
+sentences = positive['verified_reviews'].tolist()
+len(sentences)
+
+import string
+import nltk
+from nltk.corpus import stopwords
+
+# Ensure 'feedback' is the correct column name
+negative = data[data['feedback'] == 0]
+
+# Example to demonstrate punctuation and stopwords removal
+nltk.download('stopwords')
+
+mini_challenge = 'Here is a mini challenge, that will teach you how to remove stopwords and punctuation'
+
+# Remove punctuation
+challenge_no_punctuation = ''.join([char for char in mini_challenge if char not in string.punctuation])
+
+# Remove stopwords
+challenge_clean = [word for word in challenge_no_punctuation.split() if word.lower() not in stopwords.words('english')]
+
+# Output the cleaned list
+print(challenge_clean)

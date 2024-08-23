580a1833762e234b59018c29dbecdde961924cc0
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-08-23 10:00:55 +0000

    modified:   amazon_analyzis.py commit

--- amazon_analyzis.py
+++ amazon_analyzis.py
@@ -94,3 +94,17 @@ y = data['feedback']
 from sklearn.model_selection import train_test_split 
 
 X_train, X_test ,y_train , y_test = train_test_split(X,y,test_size = 0.2)
+
+from sklearn.naive_bayes import MultinomialNB
+
+NB_classifier = MultinomialNB()
+NB_classifier.fit(X_train,y_train)
+
+from sklearn.metrics import classification_report, confusion_matrix 
+
+y_predict_test = NB_classifier.predict(X_test)
+
+cm = comfusion_matrix(y_test,y_predict_test)
+
+sns.heatmap(cm,annot = True)
+

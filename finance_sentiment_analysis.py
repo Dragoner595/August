0a7c2e3b6e970360d765e8a1590d06e1bee4108a
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-08-27 12:48:16 +0000

    modified:   finance_sentiment_analysis.py commit

--- finance_sentiment_analysis.py
+++ finance_sentiment_analysis.py
@@ -20,3 +20,38 @@ data = pd.read_csv('/content/sample_data/finensial_statments.csv')
 data.head(5)
 
 # other in google colab will put it tomorrow 
+
+# will lower case all words and delete annesesery signs
+data['news_reviews'] = data['Sentence'].str.translate(str.maketrans('', '', string.punctuation)).str.lower()
+# apply function from liberary nltk for a purpose of toekenization
+data['news_reviews_tokenized'] = data['news_reviews'].apply(nltk.word_tokenize)
+
+data.head(5)
+
+# uploading english stopwords from liberary for deleting annesesary usual words from sentences
+
+english_stopwords = set(stopwords.words('english'))
+
+# function to delete all words from our column wich is similar in english stop word list
+def remove_stopwords(tokens):
+    return [word for word in tokens if word.lower() not in english_stopwords]
+
+data['clean_tokens'] = data['news_reviews_tokenized'].apply(remove_stopwords)
+
+print(data.head(5))
+
+# we will prepera new coum for future analysis
+
+data['Sentence_cleaned'] = data['clean_tokens'].apply(lambda x: ' '.join(x))
+
+data.head(5)
+
+# Flattening the List: The list comprehension [token for sublist in data['clean_tokens'] for token in sublist] flattens the list of token lists into a single list of tokens.
+# Frequency Distribution: FreqDist(all_tokens) is then used to create a frequency distribution from this flattened list.
+# Most Common Tokens: freq_dist.most_common(20) retrieves the 20 most common tokens
+
+all_tokens = [token for sublist in data['clean_tokens'] for token in sublist]
+
+freq_dist = FreqDist(all_tokens)
+
+print(freq_dist.most_common(20))
